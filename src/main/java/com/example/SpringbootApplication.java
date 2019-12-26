dcd07e70d8117cfc2265d3e8b1aec3b0c1228445
lc <522678737@qq.com>
2019-12-26 15:43:05 +0800

    新增netty测试代码

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -14,24 +14,24 @@ import java.net.InetSocketAddress;
 @MapperScan("com.example.mapper")
 public class SpringbootApplication implements CommandLineRunner {
 
-	@Value("${netty.port}")
-	private int port;
+    @Value("${netty.port}")
+    private int port;
 
-	@Value("${netty.url}")
-	private String url;
+    @Value("${netty.url}")
+    private String url;
 
-	@Autowired
-	private NettyServer server;
+    @Autowired
+    private NettyServer server;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
 
 
-	@Override
-	public void run(String... strings) throws Exception {
-		InetSocketAddress address = new InetSocketAddress(url,port);
-		server.start(address);
-	}
+    @Override
+    public void run(String... strings) throws Exception {
+        InetSocketAddress address = new InetSocketAddress(url, port);
+        server.start(address);
+    }
 }
 
